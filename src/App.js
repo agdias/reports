f85ed9f34eae2870a4f8be3390a5127963a931c9
Angelo <agdias@gmail.com>
2021-02-02 13:32:24 -0300

    Capturar erro

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { Button, Icon } from 'semantic-ui-react'
+import { 
+  BrowserRouter as Router, 
+  Switch,
+  Route,
+  Link } from 'react-router-dom'
+
+          
 import './assets/styles/index.css'
 import { authenticate } from './utils/api2'
-
-
 import Roteadores from './components/Roteadores'
 
 const  App = () => {
-    const [ user, setUser ] = useState("censored")
-    const [ password, setPassword ] = useState("censored")
+    const [ user, setUser ] = useState("Admin_Zabbix")
+    const [ password, setPassword ] = useState("Z@bB1Xx")
     const [ token, setToken ] = useState(null)
+    const [ error, setError ] = useState(null)
     useEffect(() => {
         authenticate(user,password)
           .then((token) => { 
               setToken(token)
              
             })
-          .catch((error) => console.error("Error"))
+         
+          .catch(() => setError("Falha de autenticação!"))
           
     },[])
    return (     
@@ -26,3 +33,6 @@ const  App = () => {
    )
 }
 export default App;
+
+  //const user = "Admin_Zabbix"
+    //const password = "Z@bB1Xx" 
